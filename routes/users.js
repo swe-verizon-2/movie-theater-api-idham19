dec6d148a130aa6c952b4ae606e423f4189560cd
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-26 12:31:53 -0400

    user router with all request method added

---
+++ routes/users.js
@@ -0,0 +1,123 @@
+const express = require("express");
+const { Show, User } = require("../models");
+// const {User} = require("../models/index");
+const { check, validationResult } = require("express-validator");
+
+const route = express.Router();
+
+route.get("/", async (req, res, next) => {
+  try {
+    const getAllUsers = await User.findAll({ include: Show });
+    res.json(getAllUsers);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+route.get("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const findUserById = await User.findByPk(userId, { include: Show });
+    res.status(200).json(findUserById);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+//Post request to create a new User
+route.post(
+  "/",
+  [check("showId").not().isEmpty().trim().withMessage("ShowIds is required")],
+  [
+    check("username")
+      .not()
+      .isEmpty()
+      .trim()
+      .withMessage("username is required"),
+  ],
+  [
+    check("password")
+      .not()
+      .isEmpty()
+      .trim()
+      .withMessage("password is required"),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+      }
+
+      const { username, password, showId } = req.body; //
+
+      // Create the User instance
+      const newUser = await User.create({ name: "fella", password: "1215474" });
+
+      // Find all Shows by their ids
+      const shows = await Show.findAll({
+        where: {
+          id: showId,
+        },
+      });
+
+      if (shows.length !== showId.length) {
+        return res.status(404).json({ error: "One or more show not found" });
+      }
+
+      await newUser.addShows(shows); // Using addShows for many-to-many association
+
+      res.status(201).json(newUser);
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  }
+);
+
+//Put Method
+route.put(
+  "/:id",
+  [check("showId").not().isEmpty().trim().withMessage("ShowIds is required")],
+  [
+    check("username")
+      .not()
+      .isEmpty()
+      .trim()
+      .withMessage("username is required"),
+  ],
+  [
+    check("password")
+      .not()
+      .isEmpty()
+      .trim()
+      .withMessage("password is required"),
+  ],
+  async (req, res, next) => {
+    try {
+      const userId = req.params.id;
+      const userData = req.body;
+      const findUserId = await User.findByPk(userId, { include: Show });
+      await findUserId.update(userData);
+      res.json(findUserId);
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  }
+);
+route.delete("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const findUser = await User.findByPk(userId);
+    await findUser.destroy();
+    const allUsers = await User.findAll({ include: Show });
+    res.json(allUsers);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = route;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const userRouter = require("../routes/users");
 
 app.use(express.json());
+app.use("/users", userRouter);
 
 module.exports = app;
